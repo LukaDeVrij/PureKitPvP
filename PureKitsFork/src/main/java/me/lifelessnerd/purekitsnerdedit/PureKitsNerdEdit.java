22ab84ff7f39994eb9c5bf932803007d7ff49dc5
Luka <lukadevrij@gmail.com>
2022-07-26 23:31:47 +0200

    GetKit draft

---
+++ PureKitsFork/src/main/java/me/lifelessnerd/purekitsnerdedit/PureKitsNerdEdit.java
@@ -0,0 +1,17 @@
+package me.lifelessnerd.purekitsnerdedit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PureKitsNerdEdit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
